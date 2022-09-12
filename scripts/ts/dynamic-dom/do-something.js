bfc8473bfdb82e469edee4474bf6c10c71555815
Nisha Sahgal <nisha.sahgal@wustl.edu>
2022-09-12 11:16:23 -0500

    information foraging studio

--- scripts/ts/dynamic-dom/do-something.js
+++ scripts/ts/dynamic-dom/do-something.js
@@ -1,3 +1,23 @@
 export function doSomething() {
-  console.log("Put some code in here!");
+  console.log("doing something");
+
+  // When the user scrolls the page, execute myFunction 
+  window.onscroll = function() {myFunction()};
+
+  // Get the header
+  var header = document.getElementById("main-menu-container");
+
+  // Get the offset position of the navbar
+  var sticky = header.offsetTop;
+
+  // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+  function myFunction() {
+    if (window.pageYOffset > sticky) {
+      console.log("adding sticky");
+      header.classList.add("sticky");
+    } else {
+      console.log("removing stickey");
+      header.classList.remove("sticky");
+    }
+  }
 }
